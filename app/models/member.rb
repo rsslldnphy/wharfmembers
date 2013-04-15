c1eb90c2b3a789fed9f575f3142ccd82db2b544e
Russell <russell@russelldunphy.com>
2013-04-15 21:45:40 +0100

    Some css for members form

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,7 +1,15 @@
 class Member
   include Mongoid::Document
+
   field :membership_number, type: Integer
   field :first_name, type: String
   field :last_name, type: String
   field :email, type: String
+  field :phone, type: String
+  field :address_one, type: String
+  field :address_two, type: String
+  field :address_three, type: String
+  field :postcode, type: String
+  field :email_allowed, type: Boolean
+
 end
