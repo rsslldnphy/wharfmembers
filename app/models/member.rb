9b7e987d34c23396b365b215082a9ecc0503792c
Russell <russell@russelldunphy.com>
2013-05-01 18:55:44 +0100

    Add notes field to member

--- app/models/member.rb
+++ app/models/member.rb
@@ -7,11 +7,11 @@ class Member
   field :first_name, type: String
   field :last_name, type: String
   field :email, type: String
-  field :phone, type: String
   field :address_one, type: String
   field :address_two, type: String
   field :address_three, type: String
   field :postcode, type: String
+  field :notes, type: String
   field :email_allowed, type: Boolean, default: true
 
   embeds_many :memberships
